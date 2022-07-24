7b2e37a662e7366e1873ec0958cfd068cdf15561
coleyrockin <coleyrockin@aol.com>
2022-07-24 17:02:36 -0500

    starting on index coding

---
+++ index.js
@@ -0,0 +1,31 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your reposiory?(REQUIRED!)',
+    validate nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter your repo title.');
+            return false;
+        }
+    }
+},
+{}
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
