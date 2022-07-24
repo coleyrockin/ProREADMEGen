f4d73aada798945b198534462b9e2a1efa49e257
coleyrockin <coleyrockin@aol.com>
2022-07-24 17:33:48 -0500

    Questions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const questions = [{
     type: 'input',
     name: 'title',
     message: 'What is the title of your reposiory?(REQUIRED!)',
-    validate nameInput => {
+    validate: nameInput => {
         if (nameInput) {
             return true;
         } else {
@@ -18,9 +18,64 @@ const questions = [{
         }
     }
 },
-{}
-];
+//DESCRIPTION
+{
+    type: 'input',
+    name: 'description',
+    message: 'What is the description of your repository?(REQUIRED!)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please eneter a description of the repository.');
+        }
+    }
+},
+//INSTALL QUESTION
+{
+    type: 'confirm',
+    name: 'confirmInstall',
+    message: 'Is there an installation process?'
+},
+{
+    type: 'input',
+    name: 'Installation',
+    message: 'Please list installion instructions.',
+    // If person says yes to install install process, prompt this list install instr below
+    when: ({ confirmInstall }) => {
+        if (confirmInstall) {
+            return true;
+        } else {
+            return false
+        }
+    }
+},
+{ //confirm
+    type: 'confirm',
+    name: 'confirmUsage',
+    message: 'Would you like to give instructions for using your application?',
+},
+{//if yes
+    type: 'input',
+    name: 'instructions',
+    message: 'Please list instructions for using application.',
+    when: ({ confirmUsage }) => {
+        if (confirmUsage) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
 
+{
+    type: 'confirm',
+    name: 'confirmContrinution',
+    message: 'May other developers contribute to your repository?'
+},
+
+
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
